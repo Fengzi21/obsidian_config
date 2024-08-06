6efcfc0107f07824068a4c471902f945f1d75283
Fengzi21 <iwtbags@163.com>
2024-08-06 11:33:55 +0800

    update 5 plugins

---
+++ plugins/updates/get_updates.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+
+# wget https://github.com/mokeyish/obsidian-enhancing-export/releases/download/1.10.7/obsidian-enhancing-export-.zip
+# unzip obsidian-enhancing-export-.zip
+
+# wget https://github.com/zhaohongxuan/obsidian-weread-plugin/releases/download/0.11.0/obsidian-weread-plugin-0.11.0.zip
+
+# wget https://github.com/polyipseity/obsidian-terminal/releases/download/3.15.1/obsidian-terminal.tgz
+# tar -zxvf obsidian-terminal.tgz
+
+
+# wget https://github.com/SilentVoid13/Templater/releases/download/2.4.1/main.js
+# wget https://github.com/SilentVoid13/Templater/releases/download/2.4.1/manifest.json
+# wget https://github.com/SilentVoid13/Templater/releases/download/2.4.1/styles.css
+
+
+# wget https://github.com/vslinko/obsidian-outliner/releases/download/4.8.1/main.js
+# wget https://github.com/vslinko/obsidian-outliner/releases/download/4.8.1/manifest.json
+# wget https://github.com/vslinko/obsidian-outliner/releases/download/4.8.1/styles.css
+
+
+wget https://github.com/MarkMindCkm/obsidian-markmind/releases/download/2.0.3/main.js
+wget https://github.com/MarkMindCkm/obsidian-markmind/releases/download/2.0.3/manifest.json
+wget https://github.com/MarkMindCkm/obsidian-markmind/releases/download/2.0.3/styles.css
